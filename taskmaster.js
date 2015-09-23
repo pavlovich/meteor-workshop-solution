00fb9cf5d0adad1d1d9f52be79fe88e4d4f0398c
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-09-23 19:39:20 -0400

    Converted to dynamic, spacebars templates

--- taskmaster.js
+++ taskmaster.js
@@ -1,1 +1,17 @@
+if (Meteor.isClient) {
+  Template.registerHelper('tasks', function () {
+    return [
+      {name: 'HC JSON Task 1'},
+      {name: 'HC JSON Task 2'},
+      {name: 'HC JSON Task 3'}
+    ]
+  });
+}
 
+if (Meteor.isServer) {
+  // code to run on server
+}
+
+if (Meteor.isCordova) {
+  // code to run on server
+}
