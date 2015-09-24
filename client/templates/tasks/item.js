a346a9cdb736c418baedf25f8c4993ac35e77dbc
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-09-23 20:47:52 -0400

    Implemented 'Update Task' feature.

--- client/templates/tasks/item.js
+++ client/templates/tasks/item.js
@@ -1,6 +1,21 @@
+function isSelected(item) {
+  var selected = Session.get('selectedTask');
+  return selected ? selected._id == item._id : false;
+}
+
+Template.taskItem.helpers({
+  selected: function(){
+    return isSelected(this);
+  }
+});
+
 Template.taskItem.events({
   "click .delete-button": function () {
     Tasks.remove(this._id);
     return false;
+  },
+  "click .task-item": function () {
+    Session.set('selectedTask', isSelected(this) ? null : this);
+    $('.new-task>input').focus();
   }
 });
--- client/templates/tasks/newItem.js
+++ client/templates/tasks/newItem.js
@@ -2,11 +2,29 @@ Template.newTask.events({
   "submit .new-task": function (event) {
     var text = event.target.text.value;
 
-    var task = {name: text};
-    Tasks.insert(task);
+    var task = Session.get('selectedTask') || {};
+    task.name = text;
+
+    Session.set('selectedTask', null);
+
+    if (task._id) {
+      Tasks.update(task._id, {$set: {name: task.name}});
+    } else {
+      Tasks.insert(task);
+    }
 
     event.target.text.value = "";
 
     return false;
+  }
+});
+
+Template.newTask.helpers({
+  'selectedTaskText': function(){
+    var selectedTask = Session.get('selectedTask');
+    return selectedTask ? selectedTask.name : '';
+  },
+  'placeholderText': function(){
+    return Session.get('selectedTask') ? "Enter some text for this Task description" : "Type here to add new tasks";
   }
 });
