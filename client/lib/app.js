4860fd7c525cc7242eed60182945bc79bfd8f590
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-09-24 02:42:19 -0400

    Prepare for Angular by adding conditional logic around a global value to turn meteor rendering and routing on and off.

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,17 +1,29 @@
+Meteor.usingAngular = false;
+
+UI.registerHelper('usingSpacebars', function(){
+  return !Meteor.usingAngular;
+});
+
+Iron.utils.warn = function(){};
+
 Router.configure({
-  layoutTemplate: 'taskMaster'
+  layoutTemplate: 'defaultLayout'
 });
 
-Router.route('/', function () {
-  this.render('home');
-});
-Router.route('home');
-Router.route('about');
-Router.route('tasks');
+if(!Meteor.usingAngular) {
+  Router.route('/', function () {
+    this.render('home');
+  });
+  Router.route('home');
+  Router.route('about');
+  Router.route('tasks');
+}
 
-Router.route('notFound', {
+Router.route('notFound',{
   path: /.*/,
-  action: function () {
-    this.redirect('/');
+  action: function(){
+    if(!Meteor.usingAngular) {
+      this.redirect('/');
+    }
   }
 });
